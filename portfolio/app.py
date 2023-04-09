b07da16f9bbb7a95ea13c26567d2ce3cd088e129
Daniel White <daniel.p.white@hotmail.com>
2023-04-09 00:12:46 -0500

    portfolio page initial commit

---
+++ portfolio/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_folder='assets')
+
+
+@app.route('/portfolio')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/portfolio/assets/<path:path>')
+def send_static(path):
+    return send_from_directory('assets', path)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
